279f5c958e972035bfbe43dc38cd67607406b482
[200~yassin ebelmaya <yassin.professionaldevelper@gmail.com>
2025-05-31 22:22:37 +0100

    add apdate project labo asdslam

---
+++ project labo/js/app.js
@@ -0,0 +1,10 @@
+
+
+let showmenu = document.getElementById("menu")
+let overlayer = document.querySelector('.overlayer')
+
+showmenu.addEventListener('click', function () {
+    showmenu.classList.toggle("active")
+})
+
+
