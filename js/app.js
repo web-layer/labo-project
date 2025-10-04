baa5e67745a89bfd8e16974eb1281c4918d757a5
[200~yassin ebelmaya <yassin.professionaldevelper@gmail.com>
2025-10-04 21:49:17 +0100

    apdate page

--- js/app.js
+++ js/app.js
@@ -2,10 +2,17 @@
 
 let showmenu = document.querySelector(".aside_wrapper")
 let btn = document.querySelector('.perent')
-
+let menu = document.querySelector('.main-menu')
+let btnmenu = document.querySelector('.menu-bars')
 btn.addEventListener('click', function () {
     showmenu.classList.toggle('opne')
 })
+
+btnmenu.addEventListener('click', function () {
+  menu.classList.toggle('active')
+})
+
+
 
 
 
